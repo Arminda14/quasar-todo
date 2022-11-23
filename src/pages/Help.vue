8ee1ea1d30721f9208b3e5e0fd6a7f04078c23c9
Emerson Mahumane <emerson.mahumane@gmail.com>
2022-11-23 00:33:25 -0800

    Initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lq">
+    <h5 class="q-mt-none">help</h5>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque ipsum fuga quibusdam, incidunt recusandae ullam ut veritatis quia, corporis minus enim sunt, aspernatur laudantium vero in illum neque libero pariatur.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci in quia cumque qui pariatur dolorem porro odit. A tempora aperiam provident unde placeat possimus, necessitatibus hic aut repellendus, similique eius!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
